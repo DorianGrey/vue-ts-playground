36781f30b04cac805c600a4b04170074c1f0a39b
DorianGrey <ShadowVampire@web.de>
2017-07-15 13:28:22 +0200

    Improve vueTransform.js to be able to determine different preprocessors for a script block.

--- config/jest/vueTransform.js
+++ config/jest/vueTransform.js
@@ -1,16 +1,9 @@
-/* eslint-env node */
 const path = require("path");
 const fs = require("fs-extra");
 const vueCompiler = require("vue-template-compiler");
 const vueNextCompiler = require("vue-template-es2015-compiler");
-const tsJest = require("ts-jest/preprocessor");
 
-const transforms = {
-  ts: tsJest,
-  typescript: tsJest
-};
-
-const extractHTML = (template, templatePath) => {
+function extractHTML(template, templatePath) {
   let resultHTML = "";
 
   if (!template.lang || template.lang === "resultHTML") {
@@ -22,9 +15,9 @@ const extractHTML = (template, templatePath) => {
   }
 
   return resultHTML;
-};
+}
 
-const generateOutput = (script, renderFn, staticRenderFns) => {
+function generateOutput(script, renderFn, staticRenderFns) {
   let output = "";
   output += `/* istanbul ignore next */;(function(){\n${script}\n})()\n
     /* istanbul ignore next */if (module.exports.__esModule) module.exports = module.exports.default\n`;
@@ -36,7 +29,7 @@ const generateOutput = (script, renderFn, staticRenderFns) => {
       /* istanbul ignore next */__vue__options__.staticRenderFns = ${staticRenderFns}\n`;
   }
   return output;
-};
+}
 
 const stringifyRender = render =>
   vueNextCompiler(`function render () {${render}}`);
@@ -44,49 +37,84 @@ const stringifyRender = render =>
 const stringifyStaticRender = staticRenderFns =>
   `[${staticRenderFns.map(stringifyRender).join(",")}]`;
 
+function findProcessor(filePath, transformers) {
+  let matchingTransformer;
+  for (let t of transformers) {
+    const [matcher, transformer] = t;
+    const regexp = new RegExp(matcher);
+    if (regexp.test(filePath)) {
+      matchingTransformer = transformer;
+      break;
+    }
+  }
+  return matchingTransformer;
+}
+
+function transformScript(filePath, jestConfig, transformOptions, script) {
+  let transformedScript;
+  if (script) {
+    // We have to differ between inline code and external referenced file.
+    let targetSource, targetSourcePath;
+
+    const requiresPreprocessor =
+      !!script.lang && !/js|javascript/.test(script.lang);
+
+    if (script.src) {
+      // External source - we have to read the file content before we can go ahead.
+      // The destination path has to be resolved w.r.t. the "src" entry of the
+      // script tag.
+      targetSourcePath = path.resolve(path.dirname(filePath), script.src);
+      targetSource = fs.readFileSync(targetSourcePath, "utf8");
+    } else {
+      // We need to update the file extension.
+      // TODO: Need to figure out if this simple replacement works fine.
+      targetSourcePath = filePath.replace(
+        /.vue$/,
+        script.lang ? `.${script.lang}` : ".js"
+      );
+      targetSource = script.content;
+    }
+
+    // Determine a preprocessor from the provided transform registry.
+    // If a preprocessor is required, but cannot be determined, we need to throw
+    // an error and let things crash.
+    const externalProcessor = findProcessor(
+      targetSourcePath,
+      jestConfig.transform
+    );
+
+    if (externalProcessor) {
+      const processor = require(externalProcessor);
+      transformedScript = processor.process(
+        targetSource,
+        targetSourcePath,
+        jestConfig,
+        transformOptions
+      );
+    } else if (!requiresPreprocessor) {
+      transformedScript = script.content;
+    } else {
+      throw new Error(
+        `Cannot find preprocessor for script of ${filePath}, but it requires one. Used ${targetSourcePath} for determination.`
+      );
+    }
+  }
+  return transformedScript;
+}
+
 module.exports = {
   // TODO: We need to handle "getCacheKey".
-  // TODO: We need to work around the av-ts stuff with static attributes - they are added via setting
-  // .arguments and .caller, but these are not allowed to be modified in strict mode.
-  // TODO: Since jest does not know about externally referenced files, we need a way to figure out
-  // if the "instrument" transform option should be set or not.
-  // The current way to force jest to collect coverage from .vue files causes the instrumentation to trigger,
-  // but the results might be a bit confusing...
 
   process(src, filePath, jestConfig, transformOptions) {
     // heavily based on vueify (Copyright (c) 2014-2016 Evan You)
     const { script, template } = vueCompiler.parseComponent(src, { pad: true });
 
-    // If there is a specific lang definition, but no transformer is listed, we cannot handle this ...
-    if (script && script.lang && !transforms[script.lang]) {
-      throw new Error(`No transformer defined for script.lang=${script.lang}`);
-    }
-
-    let transformer = script && script.lang ? transforms[script.lang] : null;
-    let transformedScript;
-
-    if (transformer && script) {
-      // We have to differ between inline code and external referenced file.
-      if (script.src) {
-        const targetSourcePath = path.resolve(
-          path.dirname(filePath),
-          script.src
-        );
-        const targetSource = fs.readFileSync(targetSourcePath, "utf8");
-        transformedScript = tsJest.process(
-          targetSource,
-          targetSourcePath,
-          jestConfig,
-          transformOptions
-        );
-      } else {
-        throw new Error(
-          `Transforming inline sources is currently not supported.`
-        );
-      }
-    } else if (script) {
-      transformedScript = script.content;
-    }
+    let transformedScript = transformScript(
+      filePath,
+      jestConfig,
+      transformOptions,
+      script
+    );
 
     let render;
     let staticRenderFns;
