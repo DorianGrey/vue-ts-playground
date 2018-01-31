a8253710957ec20d0f9083811be537d375b6a656
DorianGrey <ShadowVampire@web.de>
2018-01-31 10:19:36 +0100

    Fix some problems regarding determination of relative chunk names.

--- scripts/util/getRelativeChunkName.js
+++ scripts/util/getRelativeChunkName.js
@@ -5,14 +5,22 @@ function getRelativeChunkName(buildFolder, fileName) {
   // Enforce unix version to be able to properly handle everything else ...
   const unixedBuildFolder = buildFolder.split(path.sep).join("/");
 
-  return fileName
-    .split(path.sep)
-    .join("/")
-    .replace(unixedBuildFolder, "")
-    .replace(
-      /\/?(.*)(\.[0-9a-f]+)?(\.chunk)?(\.(js|css|json|webmanifest))/,
-      (match, p1, p2, p3, p4) => p1 + p4
-    );
+  let unixedFileName = fileName.split(path.sep).join("/");
+
+  // Replacing by relative path is more stable, but not always usable regarding
+  // provided relative file names...
+  // In case `fileName` is an absolute path, using `path.relative` is favorable,
+  // since it also avoids problems with potentially leading path separators.
+  if (path.isAbsolute(unixedFileName)) {
+    unixedFileName = path.relative(unixedBuildFolder, unixedFileName);
+  } else {
+    unixedFileName = unixedFileName.replace(unixedBuildFolder, "");
+  }
+
+  return unixedFileName.replace(
+    /\/?(.*)(\.[0-9a-f]{8,})(\.chunk)?(\.js|\.css|\.json|\.webmanifest)/,
+    (match, p1, p2, p3, p4) => p1 + p4
+  );
 }
 
 module.exports = getRelativeChunkName;
