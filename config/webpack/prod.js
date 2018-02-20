5cf9788d7a808edf90c3b30e4a14595f3ea5d245
DorianGrey <ShadowVampire@web.de>
2018-02-20 12:55:09 +0100

    Update lighthouse reports to include with-bulma (former) and with-vuetify (current) versions.
    + Improve Lighthouse results as well.

--- config/webpack/prod.js
+++ config/webpack/prod.js
@@ -40,18 +40,6 @@ const extractTextPluginOptions = shouldUseRelativeAssetPaths
   : {};
 
 module.exports = function() {
-  // Transformer to remove revision info in case the output file already contains
-  // a hash (i.e. webpack output).
-  const hashRegExp = /\.\w{8}\./;
-  const removeRevisionTransform = manifestEntries => {
-    return manifestEntries.map(entry => {
-      if (hashRegExp.test(entry.url)) {
-        delete entry.revision;
-      }
-      return entry;
-    });
-  };
-
   return merge.smart(
     commonConfig(false, extractTextPluginOptions, paths.publicUrl),
     {
@@ -137,8 +125,7 @@ module.exports = function() {
           globPatterns: ["**/*.{html,js,css,jpg,eot,svg,woff2,woff,ttf,json}"],
           globIgnores: ["**/*.map"],
           swDest: path.join(paths.appBuild, "service-worker.js"),
-          swSrc: path.join(paths.appSrc, "service-worker.js"),
-          manifestTransforms: [removeRevisionTransform]
+          swSrc: path.join(paths.appSrc, "service-worker.js")
         }),
 
         new UglifyJsPlugin({
