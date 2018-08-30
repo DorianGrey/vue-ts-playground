9a813704243ab68d0f3dfc4b4bab9ef8353ca046
DorianGrey <ShadowVampire@web.de>
2018-08-30 08:56:19 +0200

    Refactor SFC to use external TS files - better for linting and debugging...

--- src/views/todoList/todoListEntryDisplay.vue
+++ src/views/todoList/todoListEntryDisplay.vue
@@ -26,25 +26,7 @@
   </v-flex>
 </template>
 
-<script lang="ts">
-import Vue from "vue";
-import Component from "vue-class-component";
-import { Prop } from "vue-property-decorator";
-
-import { TodoModel } from "./state/types";
-import { TODO_MODULE_MUTATIONS, TodoSpace } from "./state/todo.state";
-
-@Component
-export default class TodoListEntryDisplay extends Vue {
-  @TodoSpace.Mutation(TODO_MODULE_MUTATIONS.SET_EDITABLE)
-  readonly setEditable: (target: number) => void;
-
-  @TodoSpace.Mutation(TODO_MODULE_MUTATIONS.DELETE)
-  readonly deleteTodo: (target: number) => void;
-
-  @Prop({ type: Object, required: true })
-  todo: TodoModel;
-}
+<script lang="ts" src="./todoListEntryDisplay.ts">
 </script>
 
 <style scoped>
